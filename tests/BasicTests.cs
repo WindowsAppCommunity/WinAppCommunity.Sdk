56bf430eb98bedcff8b0fbe633b9adc4ab0a2b7e
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-01-12 12:52:59 -0600

    Initial basic tests

---
+++ tests/BasicTests.cs
@@ -0,0 +1,19 @@
+﻿using Ipfs;
+
+namespace WinAppCommunity.Sdk.Tests;
+
+[TestClass]
+public partial class BasicTests
+{
+    [TestMethod]
+    public void TestPeerIdToCid()
+    {
+        var keyId = "12D3KooWHTJs5AGsyVqosX15r2UwUXwU3XnDb6RFPka3EMeVhhJr";
+            
+        var peer = new Peer { Id = keyId };
+
+        Cid peerCid = new Cid { Hash = keyId };
+        
+        Console.WriteLine("Success");
+    }
+}
